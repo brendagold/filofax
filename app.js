3d2536185ee5f7d500a39a67231eca3c390ab604
Chukwuma Mary <petersbrenda47@gmail.com>
2020-08-03 18:15:44 +0100

    changes made

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3020;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', require('./routes/contents'));
+app.use('/', require('./api/routes/contents'));
 
 app.listen(port, () => {
   console.log(`Express Server started on Port ${port}`);
