5b6d6feb7157a2ac81b92ea95c1ece9ec9186f5c
Chukwuma Mary <petersbrenda47@gmail.com>
2020-08-17 13:36:28 +0100

    Testing

--- test/configTest.js
+++ test/configTest.js
@@ -81,7 +81,7 @@ describe('Tasks API', () => {
       const content = {
         title: 'A New Day',
         desc: 'Today is Sunday, I am so grateful',
-        createdOn: '11:37:13 AM, Mon Aug 17 2020',
+        createdOn: '11:37:13 AM, Mon Aug 24 2020',
       };
       chai
         .request(server)
